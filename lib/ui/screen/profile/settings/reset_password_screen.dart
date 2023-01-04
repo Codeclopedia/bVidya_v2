0b30354417afc17a4a1962aa1c2659fe1b16f1be
ravikantdroid <er.cse.ravikant@gmail.com>
2023-01-04 17:30:26 +0530

    reset password

--- lib/ui/screen/profile/settings/reset_password_screen.dart
+++ lib/ui/screen/profile/settings/reset_password_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
+import '/core/utils.dart';
 import '/core/state.dart';
 import '/ui/base_back_screen.dart';
 import '/core/ui_core.dart';
@@ -11,65 +12,89 @@ class ResetPasswordScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
     return BaseWilPopupScreen(
       onBack: () async => true,
       child: BaseNoScrollSettings(
         bodyContent: Padding(
           padding: EdgeInsets.symmetric(horizontal: 6.w, vertical: 1.h),
-          child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: 4.h),
-                Text(
-                  S.current.resetTitle,
-                  style: textStyleSettingHeading,
-                ),
-                SizedBox(height: 3.h),
-                Text(
-                  S.current.fp_desc_message,
-                  style: TextStyle(
-                    fontFamily: kFontFamily,
-                    fontSize: 10.sp,
-                    color: Colors.black,
+          child: Form(
+            key: formKey,
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height: 4.h),
+                  Text(
+                    S.current.resetTitle,
+                    style: textStyleSettingHeading,
                   ),
-                ),
-                SizedBox(height: 3.h),
-                TextFormField(
-                  controller: textController,
-                  decoration: inputDirectionStyle.copyWith(
-                    hintText: S.current.emailHint,
+                  SizedBox(height: 3.h),
+                  Text(
+                    S.current.fp_desc_message,
+                    style: TextStyle(
+                      fontFamily: kFontFamily,
+                      fontSize: 10.sp,
+                      color: Colors.black,
+                    ),
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(height: 3.h),
-                Consumer(builder: (context, ref, child) {
-                  return ElevatedButton(
-                    style: elevatedButtonTextStyle,
-                    onPressed: () async {
-                      String email = textController.text;
-                      showLoading(ref);
-                      final result = await ref
-                          .read(loginRepositoryProvider)
-                          .forgetPassword(email);
-                      hideLoading(ref);
-                      if (result != null) {
-                        AppSnackbar.instance.error(context, result);
-                      } else {
-                        AppSnackbar.instance
-                            .message(context, 'Password sent to your email id');
-                        Navigator.pop(context);
-                      }
-                    },
-                    child: Text(S.current.submitBtn),
-                  );
-                }),
-                const Spacer()
-              ]),
+                  SizedBox(height: 3.h),
+                  Consumer(builder: (context, ref, child) {
+                    return TextFormField(
+                      controller: textController,
+                      decoration: inputDirectionStyle.copyWith(
+                        hintText: S.current.emailHint,
+                      ),
+                      onFieldSubmitted: (value) {
+                        if (formKey.currentState?.validate() == true) {
+                          _requidEmail(value, ref, context);
+                        }
+                      },
+                      validator: (value) {
+                        if (value?.isEmpty == true) {
+                          return S.current.login_email_empty;
+                        }
+                        if (!isValidEmail(value ?? '')) {
+                          return S.current.login_email_invalid;
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                    );
+                  }),
+                  SizedBox(height: 3.h),
+                  Consumer(builder: (context, ref, child) {
+                    return ElevatedButton(
+                      style: elevatedButtonTextStyle,
+                      onPressed: () async {
+                        if (formKey.currentState?.validate() == true) {
+                          String email = textController.text;
+                          _requidEmail(email, ref, context);
+                        }
+                      },
+                      child: Text(S.current.submitBtn),
+                    );
+                  }),
+                  const Spacer()
+                ]),
+          ),
         ),
       ),
     );
+  }
+
+  _requidEmail(String email, WidgetRef ref, BuildContext context) async {
+    showLoading(ref);
+    final result =
+        await ref.read(loginRepositoryProvider).forgetPassword(email);
+    hideLoading(ref);
+    if (result != null) {
+      AppSnackbar.instance.error(context, result);
+    } else {
+      AppSnackbar.instance.message(context, 'Password sent to your email id');
+      Navigator.pop(context);
+    }
   }
 
 // //AppColors.primaryColor
